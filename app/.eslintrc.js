cc6009291d301f541fa24e27836bf321418edd49
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-10-25 23:15:49 +0000

    fix prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -2,60 +2,59 @@
 // Licensed under the MIT license.
 
 module.exports = {
-    env: {
-      browser: true,
-      node: true,
-      es6: true
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'header', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'header', 'prettier'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 2020,
-      sourceType: 'module'
-    },
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': [
-        'warn',
-        {
-          allowExpressions: true,
-          allowTypedFunctionExpressions: true
-        }
-      ],
-      "prettier/prettier": [
-        "error",
-        {
-          "endOfLine": "auto"
-        }
-      ],
-      eqeqeq: 'warn',
-      'header/header': ['error', 'line', ' Copyright (c) Microsoft Corporation.\n Licensed under the MIT license.'],
-      'react/display-name': 'off',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
-      curly: 'error'
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
-    overrides: [
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
       {
-        files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx', '**/mocks/*'],
-        env: {
-          jest: true
-        }
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
       }
-    ]
-  };
-  
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    eqeqeq: 'warn',
+    'header/header': ['error', 'line', ' Copyright (c) Microsoft Corporation.\n Licensed under the MIT license.'],
+    'react/display-name': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    curly: 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx', '**/mocks/*'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,8 +4,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 module.exports = (env) => {
-    const babelConfig = require('./.babelrc.js');
-    const commonConfig = require('./common.webpack.config')(__dirname, env, babelConfig);
-    return commonConfig;
-  };
-  
+  const babelConfig = require('./.babelrc.js');
+  const commonConfig = require('./common.webpack.config')(__dirname, env, babelConfig);
+  return commonConfig;
+};
